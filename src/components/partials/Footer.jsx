b6621e2ec20fccd18f47da2aa9ed91f6457c9e43
RonK-0 <ronkua12@gmail.com>
2024-04-29 07:13:02 +0800

    first commit

---
+++ src/components/partials/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { baseImgUrl } from '../helpers/functions-general'
+
+const Footer = () => {
+  return (
+    <>
+      <footer className="footer">
+
+      </footer>
+    </>
+  )
+}
+
+export default Footer
