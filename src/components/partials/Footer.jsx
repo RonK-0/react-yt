ac840ae5d5cf1f1c42d8d31def763f082cc9f156
RonK-0 <ronkua12@gmail.com>
2024-04-29 15:42:07 +0800

    stll missing category buttons | 2024-04-29

--- src/components/partials/Footer.jsx
+++ src/components/partials/Footer.jsx
@@ -1,11 +1,28 @@
 import React from 'react'
-import { baseImgUrl } from '../helpers/functions-general'
+// import { baseImgUrl } from '../helpers/functions-general'
+import { Link } from 'react-router-dom'
 
 const Footer = () => {
   return (
     <>
-      <footer className="footer">
-
+      <footer className="footer f-col">
+        <div className="flex flex-wrap gap-1">
+          <small><Link to={'#'}>About</Link></small>
+          <small><Link to={'#'}>Press</Link></small>
+          <small><Link to={'#'}>Copyright</Link></small>
+          <small><Link to={'#'}>Contact</Link> us</small>
+          <small><Link to={'#'}>Creators</Link></small>
+          <small><Link to={'#'}>Advertise</Link></small>
+          <small><Link to={'#'}>Developers</Link></small>
+        </div>
+        <div className="flex flex-wrap gap-1">
+          <small><Link to={'#'}>Terms</Link></small>
+          <small><Link to={'#'}>Privacy</Link></small>
+          <small><Link to={'#'}>Policy & Safety</Link></small>
+          <small><Link to={'#'}>How YouTube works</Link></small>
+          <small><Link to={'#'}>Test new features</Link></small>
+        </div>
+        <small>&copy; 2024 Google LLC</small>
       </footer>
     </>
   )
